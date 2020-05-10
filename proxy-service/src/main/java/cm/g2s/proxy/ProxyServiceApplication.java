26937bb15463ea1f7476c38826185fe3759c3d7a
Brice amk <ambiandji@gmail.com>
2020-05-10 06:14:27 +0100

    Adding Proxy Service to gmoney-ms. Closes #3

---
+++ proxy-service/src/main/java/cm/g2s/proxy/ProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.g2s.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
---
+++ proxy-service/src/test/java/cm/g2s/proxy/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.g2s.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
