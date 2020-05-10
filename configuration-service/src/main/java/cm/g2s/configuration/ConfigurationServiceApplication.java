35798ae763240c2b0e020f9abdf082b53daade2c
Brice amk <ambiandji@gmail.com>
2020-05-10 05:49:50 +0100

    Adding Configuration Service to gmoney-ms. Closes #1

---
+++ configuration-service/src/main/java/cm/g2s/configuration/ConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.g2s.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+
+}
