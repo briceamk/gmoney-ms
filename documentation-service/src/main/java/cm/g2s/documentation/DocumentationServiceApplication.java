5b0bc1dc5f66362c45c4e27c954f1daa59b42cfd
Brice amk <ambiandji@gmail.com>
2020-05-10 06:23:26 +0100

    Adding Documentation API Service to gmoney-ms. Closes #4

---
+++ documentation-service/src/main/java/cm/g2s/documentation/DocumentationServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.g2s.documentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentationServiceApplication.class, args);
+	}
+
+}
---
+++ documentation-service/src/test/java/cm/g2s/documentation/DocumentationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.g2s.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
