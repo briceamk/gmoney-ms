215fb7ffde918546b80bc51fd66832a84b13b582
Brice amk <ambiandji@gmail.com>
2020-05-10 06:02:03 +0100

    Adding Discovery Service to gmoney-ms. Closes #2

---
+++ discovery-service/src/main/java/cm/g2s/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.g2s.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
