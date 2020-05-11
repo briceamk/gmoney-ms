122650cf6babf843598f168aab493fa4b5fc9d66
Brice amk <ambiandji@gmail.com>
2020-05-11 13:09:50 +0100

    Refactoring discovery service config

--- discovery-service/src/main/docker/run.sh
+++ discovery-service/src/main/docker/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+echo "##################################################"
+echo "Waiting for GMONEY Configuration service  to start"
+echo "##################################################"
+
+while ! $(nc -z configuration-service 7000 ); do sleep 3; done
+
+echo ">>>>>>>>>>>>>>>>> Starting GMONEY Discovery......."
 
 echo "##################################################"
 echo "GMONEY - Discovery Service"
 echo "##################################################"
-java -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar -Xmx32m -Xss256k /usr/local/discovery/discovery-service-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=$PROFILE -jar -Xmx32m -Xss256k /usr/local/discovery/@project.build.finalName@.jar
