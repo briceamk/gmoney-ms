e5bbc786564d40eadb24ae59c26eeaf2f1852d85
Brice amk <ambiandji@gmail.com>
2020-05-11 15:17:09 +0100

    Refactoring different configurations

--- proxy-service/src/main/docker/run.sh
+++ proxy-service/src/main/docker/run.sh
@@ -3,11 +3,11 @@ echo "##################################################"
 echo "Waiting for GMONEY Discovery Service to start"
 echo "##################################################"
 
-while ! $(nc -z discover-service 7010 ); do sleep 3; done
+while ! $(nc -z discovery 7010 ); do sleep 3; done
 
 echo ">>>>>>>>>>>>>>>>>>>>> Starting GMONEY Proxy......."
 
 echo "##################################################"
 echo "GMONEY - Proxy Service"
 echo "##################################################"
-java -Dspring.profiles.active=$PROFILE -jar -Xmx32m -Xss256k /usr/local/procy/@project.build.finalName@.jar
+java -Dspring.profiles.active=$PROFILE -jar -Xmx32m -Xss256k /usr/local/proxy/@project.build.finalName@.jar
