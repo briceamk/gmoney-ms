f78db01ddc7e1a8c6839dc6d3988a68e60e0cae3
Brice amk <ambiandji@gmail.com>
2020-05-13 11:55:03 +0100

    Adding Company Service. Closes #12

---
+++ company-service/src/test/java/cm/g2s/company/CompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.g2s.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
