cc2ba319e1b689c49e3eca9a66b0f8b44b08fa77
Brice amk <ambiandji@gmail.com>
2020-05-13 16:02:21 +0100

    Added Account Service and Dockerfile. Closes #17

---
+++ account-service/src/main/docker/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "##################################################"
+echo "Waiting for GMONEY Configuration service  to start"
+echo "##################################################"
+
+while ! $(nc -z configuration 7000 ); do sleep 3; done
+
+echo "##################################################"
+echo "Waiting for GMONEY Discovery service  to start"
+echo "##################################################"
+
+while ! $(nc -z discovery 7000 ); do sleep 3; done
+
+echo ">>>>>>>>>>>>>>>>> Starting GMONEY Company......."
+
+echo "##################################################"
+echo "GMONEY - Account Service"
+echo "##################################################"
+java -Dspring.profiles.active=$PROFILE -jar -Xmx32m -Xss256k /usr/local/account/@project.build.finalName@.jar
