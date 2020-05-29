985f5e00545f0c944ab80a5818b13c8e44fb13df
Brice amk <ambiandji@gmail.com>
2020-05-29 15:09:26 +0100

    Adding Mail Template Authorities

---
+++ uaa-service/src/main/resources/sql/data_perm_mailtemplate.sql
@@ -0,0 +1,66 @@
+/*********************************************************************************************************************
+TABLE PERMISSION
+*********************************************************************************************************************/
+
+INSERT INTO permission (id, created_date, name)
+SELECT
+ 'CREATE_MAIL_TEMPLATE_ID',current_timestamp,'CREATE_MAIL_TEMPLATE'
+WHERE NOT EXISTS (
+    SELECT * FROM permission WHERE id = 'CREATE_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO permission (id, created_date, name)
+SELECT
+ 'UPDATE_MAIL_TEMPLATE_ID',current_timestamp,'UPDATE_MAIL_TEMPLATE'
+WHERE NOT EXISTS (
+    SELECT * FROM permission WHERE id = 'UPDATE_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO permission (id, created_date, name)
+SELECT
+ 'READ_MAIL_TEMPLATE_ID',current_timestamp,'READ_MAIL_TEMPLATE'
+WHERE NOT EXISTS (
+    SELECT * FROM permission WHERE id = 'READ_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO permission (id, created_date, name)
+SELECT
+ 'DELETE_MAIL_TEMPLATE_ID',current_timestamp,'DELETE_MAIL_TEMPLATE'
+WHERE NOT EXISTS (
+    SELECT * FROM permission WHERE id = 'DELETE_MAIL_TEMPLATE_ID'
+);
+
+
+/***************************
+ROLE_ADMIN
+***************************/
+INSERT INTO role_permission (role_id, permission_id)
+    SELECT
+        '1d053720-8032-4733-92bc-48b7bb1014d3', 'CREATE_MAIL_TEMPLATE_ID'
+WHERE NOT EXISTS (
+    SELECT * FROM role_permission WHERE role_id = '1d053720-8032-4733-92bc-48b7bb1014d3' AND permission_id = 'CREATE_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO role_permission (role_id, permission_id)
+    SELECT
+        '1d053720-8032-4733-92bc-48b7bb1014d3', 'UPDATE_MAIL_TEMPLATE_ID'
+WHERE NOT EXISTS (
+    SELECT * FROM role_permission WHERE role_id = '1d053720-8032-4733-92bc-48b7bb1014d3' AND permission_id = 'UPDATE_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO role_permission (role_id, permission_id)
+    SELECT
+        '1d053720-8032-4733-92bc-48b7bb1014d3', 'READ_MAIL_TEMPLATE_ID'
+WHERE NOT EXISTS (
+    SELECT * FROM role_permission WHERE role_id = '1d053720-8032-4733-92bc-48b7bb1014d3' AND permission_id = 'READ_MAIL_TEMPLATE_ID'
+);
+
+INSERT INTO role_permission (role_id, permission_id)
+    SELECT
+        '1d053720-8032-4733-92bc-48b7bb1014d3', 'DELETE_MAIL_TEMPLATE_ID'
+WHERE NOT EXISTS (
+    SELECT * FROM role_permission WHERE role_id = '1d053720-8032-4733-92bc-48b7bb1014d3' AND permission_id = 'DELETE_MAIL_TEMPLATE_ID'
+);
+
+
+
