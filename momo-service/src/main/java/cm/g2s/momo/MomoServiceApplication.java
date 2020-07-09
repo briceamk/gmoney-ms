37cc3ff3a86810fd73acadee7b9faf2a12b7c700
Brice amk <ambiandji@gmail.com>
2020-07-09 14:19:27 +0100

    Adding MoMo Service. closes #39

---
+++ momo-service/src/main/java/cm/g2s/momo/MomoServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.g2s.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoServiceApplication.class, args);
+	}
+
+}
---
+++ momo-service/src/test/java/cm/g2s/momo/MomoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.g2s.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
