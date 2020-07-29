424b528afb2ca57d5d3870b3c8ddf4a650291935
Brice amk <ambiandji@gmail.com>
2020-07-29 23:25:45 +0100

    updating docker run file

--- account-service/src/main/docker/run.sh
+++ account-service/src/main/docker/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+echo "##################################################"
 echo "Waiting for GMONEY Configuration service  to start"
 echo "##################################################"
 
 while ! $(nc -z configuration 7000 ); do sleep 3; done
 
 echo "##################################################"
-echo "Waiting for GMONEY Discovery service  to start"
+echo "Waiting for GMONEY Discovery Service to start"
 echo "##################################################"
 
 while ! $(nc -z discovery 7010 ); do sleep 3; done
