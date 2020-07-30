79cba8d5be4c0947ee2ae890f3f641a1d4e599c6
Brice amk <ambiandji@gmail.com>
2020-07-30 02:43:19 +0100

    updating docker run file

--- cron-service/src/main/docker/run.sh
+++ cron-service/src/main/docker/run.sh
@@ -16,11 +16,11 @@ echo "##################################################"
 echo "Waiting for GMONEY Proxy service  to start"
 echo "##################################################"
 
-while ! $(nc -z rule 8100 ); do sleep 3; done
+while ! $(nc -z proxy 8100 ); do sleep 3; done
 
 echo ">>>>>>>>>>>>>>>>> Starting GMONEY Cron......."
 
 echo "##################################################"
 echo "GMONEY - Cron Service"
 echo "##################################################"
-java -Dspring.profiles.active=$PROFILE -jar -Xmx32m -Xss256k /usr/local/cron/@project.build.finalName@.jar
+java -Dspring.profiles.active=$PROFILE -jar -Xmx2048m -Xss256k /usr/local/cron/@project.build.finalName@.jar
