95f9b721482dd7531df35013d7a5afa33cae1e66
Brice amk <ambiandji@gmail.com>
2020-09-13 14:59:57 +0100

    Implementing momo service in transaction service just for test

--- transaction-service/src/main/java/cm/g2s/transaction/TransactionServiceApplication.java
+++ transaction-service/src/main/java/cm/g2s/transaction/TransactionServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @EnableEurekaClient
@@ -18,6 +21,15 @@ public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+	//TODO This is just for test momo service in transaction service. remove it after test is complete
+	@Bean
+	public RestTemplate restTemplate() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(5000);
+		clientHttpRequestFactory.setConnectionRequestTimeout(5000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 }
